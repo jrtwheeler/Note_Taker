9ef9211b78cf4ee9d9249acf2dfb42466d997d85
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-04 22:10:52 -0500

    Added app.use(express.static('public'));

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-var path = require("path");
-var fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 const notes = require('./data/tabledata');
 const waitlist = require('./data/waitlist');
 
@@ -9,14 +9,15 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Route Handling
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // APIs
@@ -28,7 +29,7 @@ app.post("/api/waitlist", function(req, res) {
   return res.json(waitlist);
 })
 
-// Initializing Server
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
