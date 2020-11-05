59c9a6aa615948eec9facbb0551b94fab1bb2d30
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-04 21:45:50 -0500

    Added server.js file

---
+++ public/assets/js/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var path = require("path");
+const table = require('./data/tabledata');
+const waitlist = require('./data/waitlist');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route Handling
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/table", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/" + "table.html"));
+});
+
+// APIs
+app.get("/api/reservations", function(req, res) {
+  return res.json(table);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
+})
+
+// Initializing Server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
