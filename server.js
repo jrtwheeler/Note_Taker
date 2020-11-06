01e6132a1c61cb18bcf8349517629294d3e90de5
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-05 22:10:55 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// IP: SOURCE_ADDR:network:hardware_address:protocol_port||DEST_ADDR:network:hardware_address:protocol_port 
-
-// while(true){
-// listen for socketAPI
-// if GET request on PORT:protocol_port rooute "/"
-// use information in headers of request
-// get a website
-// use file system to get html file
-// return the html file to the source IP address
-// };
 // Dependencies
 // =============================================================
 const express = require("express");
@@ -20,13 +10,18 @@ const app = express();
 const PORT = 8000;
 
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+//Require html and API routes
+// =============================================================
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
 
+//Listener
+// =============================================================
 app.listen(PORT, function () {
   console.log("http://localhost:" + PORT);
 });
