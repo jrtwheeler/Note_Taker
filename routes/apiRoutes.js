b269de8d7963486d570ebe081944bcf47f4f2daf
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-05 21:59:39 -0500

    Added notes to each file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,35 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+const path = require("path");
+const fs = require("fs");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+
+
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), function (err, data) {
+            if (err) throw err;
+            var jsonData = JSON.parse(data);
+
+
+            console.log(jsonData);
+            return jsonData;
+        });
+        console.log("Working code block")
+    });
+
+    app.post("/api/notes", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var notes = req.body;
+
+        console.log(notes);
+
+    });
+}
--- server.js
+++ server.js
@@ -25,27 +25,7 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 require("./routes/htmlRoutes")(app);
-
-app.get("/api/notes", function(req, res) {
-  fs.readFile(path.join(__dirname, "../db/db.json"), function(err, data) {
-      if (err) throw err;
-      var jsonData = JSON.parse(data);
-
-      
-      console.log(jsonData);
-      return jsonData;
-    });
-    console.log("Working code block")
-});
-
-app.post("/api/notes", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var notes = req.body;
-
-  console.log(notes);
-
-});
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("http://localhost:" + PORT);
