66ee31fc51481b8921e78ea827288c9f96c9c80e
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-08 16:58:16 -0500

    Revisions for heroku deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 3001;;
 
 // Sets up the Express app to handle data parsing
 // =============================================================
